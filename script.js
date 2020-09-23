6e72052a10f3e20950a510dc56c7e1c1f1b471d2
Taylor MJ <taylorjohnsoninc@gmail.com>
2020-09-23 00:48:42 -0500

    updated json

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Write your JavaScript code here!
 
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
+<h3>Mission Destination</h3>
 <ol>
    <li>Name: ${}</li>
    <li>Diameter: ${}</li>
@@ -81,9 +81,29 @@ window.addEventListener("load", function() {
        success.innerHTML = "Shuttle is ready for launch";
        success.style.color = "green";
      }
- 
-    }
+     
+   fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
+   // Access the JSON in the response
+   response.json().then( function(json) {
+       console.log(json[0]);
+       const div = document.getElementById("missionTarget");
+       // Add HTML that includes the JSON data
+       div.innerHTML = `
+          <h2>Mission Destination</h2>
+          <ol>
+          <li>Name: ${json[0].name}</li>
+          <li>Diameter: ${json[0].diameter}</li>
+          <li>Star: ${json[0].star}</li>
+          <li>Distance from Earth: ${json[0].distance}</li>
+          <li>Number of Moons: ${json[0].moons}</li>
+          </ol>
+          <img src="${json[0].image}">
+         `;
+      } );
+   });
+
+   }
     
-    button.addEventListener("click", change);
+   button.addEventListener("click", change);
    
- });
+});
